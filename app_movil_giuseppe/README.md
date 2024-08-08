e15887d3f47b7b0b95e632d9cbaa6a842ec09b66
Luis Xavier Espinosa Meza <lxespinosa@espe.edu.ec>
2024-08-07 20:59:03 -0500

    app_movil

---
+++ app_movil_giuseppe/README.md
@@ -0,0 +1,3 @@
+# app_movil_giuseppe
+
+A new Flutter project.
---
+++ app_movil_giuseppe/android/app/src/main/kotlin/com/example/app_movil_giuseppe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_movil_giuseppe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
